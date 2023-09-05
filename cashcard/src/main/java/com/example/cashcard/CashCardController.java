f8095a56e36cc39d56b799f5eadffc27f191b681
INTONIGHT <tyler.throop@gmail.com>
2023-09-05 09:13:52 -0700

    page sorting added

--- cashcard/src/main/java/com/example/cashcard/CashCardController.java
+++ cashcard/src/main/java/com/example/cashcard/CashCardController.java
@@ -4,6 +4,8 @@ package com.example.cashcard;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -43,9 +45,11 @@ public class CashCardController {
     @GetMapping()
     public ResponseEntity<Iterable<CashCard>> findAll(Pageable pageable){
     	Page<CashCard> page = cashCardRepository.findAll(
-    			PageRequest.of(pageable.getPageNumber(), 
-    					pageable.getPageSize())
-    			);
+    			PageRequest.of(
+    					pageable.getPageNumber(), 
+    					pageable.getPageSize(),
+    					pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+    					));
     	return ResponseEntity.ok(page.getContent());
     }
 }
