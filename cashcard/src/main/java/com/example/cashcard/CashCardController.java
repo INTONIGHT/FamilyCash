45e8ddcd497681f8bc1467d672a95ea2231bdba9
INTONIGHT <tyler.throop@gmail.com>
2023-09-13 09:38:24 -0700

    Starting put implementation

--- cashcard/src/main/java/com/example/cashcard/CashCardController.java
+++ cashcard/src/main/java/com/example/cashcard/CashCardController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -54,5 +55,9 @@ public class CashCardController {
     					pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
     					));
     	return ResponseEntity.ok(page.getContent());
+    }
+    @PutMapping("/{requestedId}")
+    private ResponseEntity<Void> putCashCard(@PathVariable Long requestedId, @RequestBody CashCard cashCardUpdate){
+    	return ResponseEntity.noContent().build();
     }
 }
