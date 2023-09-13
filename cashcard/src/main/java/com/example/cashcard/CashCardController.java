073250b9663fa700e62db9b62b5212b06d4c86b8
INTONIGHT <tyler.throop@gmail.com>
2023-09-13 12:15:09 -0700

    Finishing up the practice now I plan on practicing with the idea I had might take a bit to get that one to a spring application

--- cashcard/src/main/java/com/example/cashcard/CashCardController.java
+++ cashcard/src/main/java/com/example/cashcard/CashCardController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,5 +66,13 @@ public class CashCardController {
         	return ResponseEntity.noContent().build();
     	}
     	return ResponseEntity.notFound().build();
+    }
+    @DeleteMapping("/{id}")
+    private ResponseEntity<Void> deleteCashCard(@PathVariable Long id, Principal principal){
+    	if(!cashCardRepository.existsByIdAndOwner(id, principal.getName())) {
+    		return ResponseEntity.notFound().build();
+    	}
+    	cashCardRepository.deleteById(id);
+    	return ResponseEntity.noContent().build();
     }
 }
